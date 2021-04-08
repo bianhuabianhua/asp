55adb47bbe9f9d661f4798d1acd3a2528d2d1b51
bianhua <3475209220@qq.com>
2021-04-08 11:17:53 +0800

    v1.0

---
+++ RazorPagesMovie/RazorPagesMovie/AppDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace RazorPagesMovie
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
---
+++ RazorPagesMovie/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace RazorPagesMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var CONN = Environment.GetEnvironmentVariable("CONN");
+            MySqlConnection conn = new MySqlConnection(CONN);//!!!!no referrence bracket variable CONN
+            conn.Open();
+            string op = "CREATE TABLE `new_schema`.`new` (`ID` INT NOT NULL);INSERT INTO new(ID) VALUES (200)"; //creat table named new;insert column ID and variable 200
+            MySqlCommand command = new MySqlCommand(op, conn);
+            //command.Parameters.AddWithValue("@ID", 200);
+            command.ExecuteNonQuery();
+           
+        }
+    }
+}
